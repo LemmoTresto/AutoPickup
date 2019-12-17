3668a6b3e3709189fdd48731c423af277f35e153
Max Berkelmans <maxberkelmans@Live.nl>
2019-12-17 23:35:04 +0100

    optimize imports

--- src/main/java/io/felux/autopickup/listener/MineListener.java
+++ src/main/java/io/felux/autopickup/listener/MineListener.java
@@ -1,16 +1,12 @@
 package io.felux.autopickup.listener;
 
 import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MineListener implements Listener {
 
